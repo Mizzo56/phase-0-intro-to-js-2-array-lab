bad049f06c2f76bfbad064aeae755498801f16fd
Elijah Mazzella <Mizzo56@gmail.com>
2024-05-09 13:56:42 -0400

    Completed Labs Arrays

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+cats.length
+function destructivelyAppendCat(catName){cats.push(catName)}
+;
+function destructivelyPrependCat(catName){cats.unshift(catName)};
+function destructivelyRemoveLastCat(catName){cats.pop(catName)};
+function destructivelyRemoveFirstCat(catName){cats.shift(catName)};
+
+function appendCat(name){
+const copyOfCats = [...cats]
+copyOfCats.push(name)
+return copyOfCats
+}
+function prependCat(name){
+const copyOfCats = [...cats]
+copyOfCats.unshift(name)
+    return copyOfCats
+}
+function removeLastCat(name){
+const copyOfCats = [...cats]
+copyOfCats.pop(name)
+return copyOfCats
+}
+function removeFirstCat(name){
+    const copyOfCats = [...cats]
+    copyOfCats.shift(name)
+    return copyOfCats
+}
